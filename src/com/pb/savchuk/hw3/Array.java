64801ab54a7b5a14151a9cd2804da7631774e9f5
Oleg Savchuk <ol.sav4yk@gmail.com>
2021-10-25 22:04:24 +0300

    add sort

--- src/com/pb/savchuk/hw3/Array.java
+++ src/com/pb/savchuk/hw3/Array.java
@@ -6,7 +6,6 @@ public class Array {
     public static void main(String[] args) {
 
         int sum = 0;
-        int neg = 0;
         int poz = 0;
 
         Scanner scaner=new Scanner(System.in);
@@ -16,24 +15,42 @@ public class Array {
             System.out.println("Введіть " + (i + 1) + " елемент масиву:");
             array[i] = scaner.nextInt();
         }
+        int neg = 0;
         int min = array[0];
         int max = array[0];
+        System.out.println("Ведений масив має вигляд: ");
         for (int i = 0; i < array.length; i++) {
-
-
             System.out.print(array[i]+" ");
-            sum = sum + array[i];
+            sum += array[i];
 
-            if (array[i] < min)
+            if (array[i] < min) {
                 min = array[i];
-            if (array[i] > max)
+            }
+            if (array[i] > max) {
                 max = array[i];
-            if (array[i] < 0);
-            neg++;
-            if (array[i] > 0);
-            poz++;
+            }
+            if (array[i] < 0) {
+                neg++;
+            }
+            if (array[i] > 0) {
+                poz++;
+            }
         }
 
+        for (int out = array.length - 1; out >= 1; out--) {
+            for (int i = 0; i < out; i++) {
+                if (array[i] > array[i + 1]) {
+                    int swap = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = swap;
+
+                }
+            }
+        }
+        System.out.println("\nВідсортований масив має вигляд: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
         System.out.println("\nСума всіх елементів масиву = " + sum);
         System.out.println("Мінімальне значення масиву = " + min);
         System.out.println("Максимальне значення масиву = " + max);
